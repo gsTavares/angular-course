a304bb94089ebb9125b2593fbbe14c7ff02d3f27
Gustavo Tavares <gustavo.tavares24@outlook.com>
2022-10-10 15:28:35 -0300

    class 18 - http requests using HttpClient and json-server

--- angular-course/src/app/components/list-render/list-render.component.ts
+++ angular-course/src/app/components/list-render/list-render.component.ts
@@ -11,18 +11,14 @@ export class ListRenderComponent implements OnInit {
 
   // Interfaces are important to maintain code pattern in scalable applications
 
-  animals: Animal[] = [ // : Animal[] -> with this annotation, 
-  // all elements in 'animals' must contain all Animal interface props
-    { name: "Turca", type: "Dog", age: 4 },
-    { name: "Tom", type: "Cat", age: 10 },
-    { name: "Frida", type: "Dog", age: 5 },
-    { name: "Bob", type: "Horse", age: 1 }
-  ]
+  animals: Animal[] = [];
 
   animalDetails: string = "";
 
   // Defining a service to the component
-  constructor(private listService: ListRenderService) { }
+  constructor(private listService: ListRenderService) {
+      this.getAnimals();
+   }
 
   ngOnInit(): void {
   }
@@ -33,6 +29,10 @@ export class ListRenderComponent implements OnInit {
 
   removeAnimal(animal: Animal): void {
      this.animals = this.listService.remove(this.animals, animal);
+  }
+
+  getAnimals(): void {
+    this.listService.getAll().subscribe(animals => this.animals = animals);
   }
 
 }
