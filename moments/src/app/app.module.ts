d05bba77c8de734f2880e0c8aae75d9670f35787
gsTavares <gustavo.tavares24@outlook.com>
2022-10-12 09:41:44 -0300

    feat: add HttpClientModule

--- moments/src/app/app.module.ts
+++ moments/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -25,7 +26,8 @@ import { MomentFormComponent } from './components/moment-form/moment-form.compon
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
