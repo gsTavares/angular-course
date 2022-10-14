3af377579111cfc2f31c3647a577c41d67766af4
Gustavo Tavares <gustavo.tavares24@outlook.com>
2022-10-14 15:53:41 -0300

    class 31 - finish message component + about page

--- moments/src/app/components/pages/new-moment/new-moment.component.ts
+++ moments/src/app/components/pages/new-moment/new-moment.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Moment } from 'src/app/models/Moment';
 import { MomentService } from 'src/app/services/moment.service';
+import { MessagesService } from 'src/app/services/messages.service';
 
 @Component({
   selector: 'app-new-moment',
@@ -11,7 +13,7 @@ export class NewMomentComponent implements OnInit {
 
   btnText = 'Compartilhar!';
 
-  constructor(private momentService: MomentService) { }
+  constructor(private momentService: MomentService, private messagesService: MessagesService, private router: Router) { }
 
   ngOnInit(): void {
   }
@@ -32,7 +34,13 @@ export class NewMomentComponent implements OnInit {
     //   formData.append('image', moment.image);
     // }
 
+    const message: String = '';
+
     await this.momentService.createMoment(moment).subscribe();
+
+    this.messagesService.add("Momento criado com sucesso");
+
+    this.router.navigate(["/"]);
   }
 
 }
