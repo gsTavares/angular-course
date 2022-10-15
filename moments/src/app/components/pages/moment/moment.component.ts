568a9f699a3e1046b9b7dabc1c537fda29252a7b
Gustavo Tavares <gustavo.tavares24@outlook.com>
2022-10-15 09:12:50 -0300

    class 35 - get individual moment

---
+++ moments/src/app/components/pages/moment/moment.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { MomentService } from 'src/app/services/moment.service';
+import { Moment } from 'src/app/models/Moment';
+import { Router, ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-moment',
+  templateUrl: './moment.component.html',
+  styleUrls: ['./moment.component.css']
+})
+export class MomentComponent implements OnInit {
+
+  moment?: Moment;
+
+  constructor(private momentService: MomentService, private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    const id: number = Number(this.route.snapshot.paramMap.get("id"));
+    this.momentService.getMomentById(id).subscribe(response => {
+      this.moment = response.body;
+    })
+  }
+
+}
